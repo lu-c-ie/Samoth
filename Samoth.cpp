2ef2a20bb635a38d317e2ae6423f02b68a82b4bb
Tom Shea <tomjonshea@gmail.com>
2016-09-11 15:30:51 -0700

    tom simplifies the spell

--- Samoth.cpp
+++ Samoth.cpp
@@ -1,44 +1,33 @@
 #include "FastLED.h"
 
+// Hardware definitions:
 #define DATA_PIN    6
 #define LED_TYPE    WS2811
 #define COLOR_ORDER GRB
 #define NUM_LEDS    73
 
+// Software settings
 #define BRIGHTNESS         255
-#define FRAMES_PER_SECOND  120
-#define UPDATES_PER_SECOND 30
 
-
-
-
+// Instantiate color array object, which is the code-representation of the
+// physical pixel strip.
+CRGB leds[NUM_LEDS];
 
 void setup() {
-
-  // tell FastLED about the LED strip configuration
-  FastLED.addLeds<LED_TYPE,DATA_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
-  //FastLED.addLeds<LED_TYPE,DATA_PIN,CLK_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
-
-
+    // tell FastLED about the LED strip configuration
+    FastLED.addLeds<LED_TYPE,DATA_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+    FastLED.setBrightness(BRIGHTNESS);
 }
 
 void loop() {
-  theaterChase(0xff,0,0,50);
+    testChaser();
 }
 
-void theaterChase(byte red, byte green, byte blue, int SpeedDelay) {
-  for (int j=0; j<10; j++) {  //do 10 cycles of chasing
-    for (int q=0; q < 3; q++) {
-      for (int i=0; i < NUM_LEDS; i=i+3) {
-        setPixel(i+q, red, green, blue);    //turn every third pixel on
-      }
-      showStrip();
-
-      delay(SpeedDelay);
-
-      for (int i=0; i < NUM_LEDS; i=i+3) {
-        setPixel(i+q, 0,0,0);        //turn every third pixel off
-      }
+void testChaser() {
+    for (int i = 0; i < NUM_LEDS; i++) {
+        leds[i] = CRGB::Red;
+        FastLED.show();
+        delay(20);
+        FastLED.clear();
     }
-  }
 }
