666aaf39797f2e1c5e597c3cce14b2cf15e36a94
Lu C Ie <lucyirvine@Lucys-MacBook-Pro.local>
2016-09-11 23:14:35 +0100

    actual code

--- Samoth.cpp
+++ Samoth.cpp
@@ -1,3 +1,44 @@
-awjfoj
+#include "FastLED.h"
 
-afjz
+#define DATA_PIN    6
+#define LED_TYPE    WS2811
+#define COLOR_ORDER GRB
+#define NUM_LEDS    73
+
+#define BRIGHTNESS         255
+#define FRAMES_PER_SECOND  120
+#define UPDATES_PER_SECOND 30
+
+
+
+
+
+void setup() {
+
+  // tell FastLED about the LED strip configuration
+  FastLED.addLeds<LED_TYPE,DATA_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+  //FastLED.addLeds<LED_TYPE,DATA_PIN,CLK_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+
+
+}
+
+void loop() {
+  theaterChase(0xff,0,0,50);
+}
+
+void theaterChase(byte red, byte green, byte blue, int SpeedDelay) {
+  for (int j=0; j<10; j++) {  //do 10 cycles of chasing
+    for (int q=0; q < 3; q++) {
+      for (int i=0; i < NUM_LEDS; i=i+3) {
+        setPixel(i+q, red, green, blue);    //turn every third pixel on
+      }
+      showStrip();
+
+      delay(SpeedDelay);
+
+      for (int i=0; i < NUM_LEDS; i=i+3) {
+        setPixel(i+q, 0,0,0);        //turn every third pixel off
+      }
+    }
+  }
+}
